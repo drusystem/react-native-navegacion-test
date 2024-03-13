cfeda375ca1ca3c8c2e7f9950c1a46220256ac09
DruQuispe <drusystem@gmail.com>
2024-03-13 09:21:42 -0500

    useSafeAreaInserts: margenes seguros de la pantalla

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {Pressable, Text, View} from 'react-native';
 import { globalStyles } from '../../theme/theme';
-import { type NavigationProp, useNavigation } from '@react-navigation/native';
+import { type NavigationProp, useNavigation, DrawerActions } from '@react-navigation/native';
 import { PrimaryButton } from '../../components/PrimaryButton';
 import { type RootStackParams } from '../../routes/StackNavigator';
 
 export const HomeScreen = () => {
 
   const navigation = useNavigation<NavigationProp<RootStackParams>>()
+
+  useEffect(()=>{
+    navigation.setOptions({
+      headerLeft:()=>(
+        <Pressable onPress={()=>navigation.dispatch(DrawerActions.toggleDrawer)}>
+          <Text>Menu</Text>
+        </Pressable>
+      )
+    })
+  },[])
 
   return (
     <View style={globalStyles.container}>
--- src/presentation/screens/profile/ProfileScreen.tsx
+++ src/presentation/screens/profile/ProfileScreen.tsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { PrimaryButton } from '../../components/PrimaryButton';
+import { DrawerActions, useNavigation } from '@react-navigation/native';
 
 export const ProfileScreen = () => {
 
+    const {top} = useSafeAreaInsets();
+    const navigation =useNavigation();
+
     return (
-        <View>
-            <Text>ProfileScreen</Text>
+        <View style={{
+            flex:1,
+            paddingHorizontal:20,
+            marginTop:top
+        }}>
+            <Text style={{marginBottom:10}}>ProfileScreen</Text>
+
+            <PrimaryButton
+            label='Abrir Menú'
+                onPress={()=>navigation.dispatch(DrawerActions.toggleDrawer)}
+            />
         </View>
     )
 }
